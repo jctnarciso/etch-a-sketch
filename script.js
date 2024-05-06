b5904ceb62335b83078019fef902b3fd88813a6a
jctnarciso <121702105+jctnarciso@users.noreply.github.com>
2024-05-07 07:11:11 +0800

    Add function to increment cell opacity

--- script.js
+++ script.js
@@ -21,3 +21,13 @@ for (let i = 0; i < 900; i++) {
   cell.classList.toggle("cell");
   grid.appendChild(cell);
 }
+
+function getOpacity(element) {
+  let styles = getComputedStyle(element);
+  return styles.opacity;
+}
+
+function incrementOpacity(element) {
+  let currentOpacity = +getOpacity(element);
+  return currentOpacity + 0.1;
+}
